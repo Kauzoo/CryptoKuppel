ac9d41a4a67046861d69e9740f8ceeb87a83705e
nyr <kauzoo@web.de>
2023-09-20 19:32:13 +0200

    Added Car prefab

---
+++ CryproKuppel/Assets/CarEngine.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CarEngine : MonoBehaviour
+{
+    public bool useCord;
+    public Transform start;
+    public Transform end;
+    public Vector3 startCord;
+    public Vector3 endCord;
+    public float speed = 1.0f; 
+
+    private float startTime;
+    private float journeyLength;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useCord)
+        {
+            startCord = start.position;
+            endCord = end.position;
+        }
+
+        var sMesh = start.gameObject.GetComponent<MeshRenderer>();
+        var eMesh = end.gameObject.GetComponent<MeshRenderer>();
+
+        sMesh.enabled = false;
+        eMesh.enabled = false;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startCord, endCord);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startCord, endCord, fractionOfJourney);
+        
+        // Reset on end reached
+        if (transform.position == endCord)
+        {
+            startTime = Time.time;
+            transform.position = startCord;
+        }
+    }
+}
