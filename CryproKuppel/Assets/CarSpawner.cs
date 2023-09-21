2100eefc769427d1c155760c1a3e5ebd960834fb
nyr <kauzoo@web.de>
2023-09-21 15:19:46 +0200

    WIP: CarSpawner

---
+++ CryproKuppel/Assets/CarSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    
+    public Transform startMarker;
+    public Transform endMarker;
+
+    public int carCount;
+    public float minGap;
+    public float maxGap;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
